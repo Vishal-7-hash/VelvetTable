ddce46b848279d543ae06c49c9752b4c3631ec07
Vishal Sharma <vishuvs2412@gmail.com>
2026-09-02 22:30:42 +0530

    Basic File Structure Changes

--- server/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createReview, getRestaurantReviews } = require('../controllers/reviewController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/').post(protect, createReview);
-router.route('/:restaurantId').get(getRestaurantReviews);
-
-module.exports = router;
